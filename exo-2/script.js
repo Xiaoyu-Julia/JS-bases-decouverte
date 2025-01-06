1b2a7432660e1b738e7f8af464902a455141c31b
Xiaoyu-Julia <xiaoyu.ma@le-campus-numerique.fr>
2025-01-07 00:03:30 +0100

    exo-2

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,20 @@
 // you can write js here
-console.log('exo-2');
+const myDate = new Date();
+let msgWeek = "pas encore le weekend";
+switch (msgWeek) {
+    case 'lundi' : 
+       console.log('pas du tout le weekend');
+       break;
+    case 'mardi' : 
+       console.log('pas encore le weekend');
+       break;
+    case 'mercredi' : 
+       console.log('bientôt le weekend');
+       break;
+    case 'mardi' : 
+       console.log('presque le weekend');
+       break;
+}
+let msgWeekend = "c'est le weekend";
+let today = myDate.getDay();
+console.log(today);
