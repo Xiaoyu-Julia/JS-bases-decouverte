749cc898e1c5f763ca938135f81e4b2ef4ac9134
Xiaoyu-Julia <xiaoyu.ma@le-campus-numerique.fr>
2025-01-06 22:11:42 +0100

    exo-1, sans prompt

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,13 @@
 // you can write js here
-console.log('hello from file');
+
+        var kelvins = 294;
+        console.log(kelvins);
+
+        var celsius = kelvins - 273; // Je convertis la température kelvins en celsius et crée un variable celsius pour stocker le résultat. La température en Celsius est inférieure de 273 degrés à celle en Kelvins.
+        console.log(celsius);
+
+        var fahrenheits = celsius * (9/5) + 32; // Je calcule la température en Fahrenheits et crée un variable fahrenheits pour stocker le résultat de calcule.
+        console.log(fahrenheits);
+
+        Math.floor(fahrenheits); // J'utilise la methode math.floor() pour arrondir le résultat obtenu.
+        console.log(Math.floor(fahrenheits));
