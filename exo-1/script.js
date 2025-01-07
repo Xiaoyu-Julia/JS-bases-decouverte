6792651c97c55a7a7465c126067e1155cabbed3d
Xiaoyu-Julia <xiaoyu.ma@le-campus-numerique.fr>
2025-01-07 10:21:26 +0100

    code optimisé

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,15 +1,19 @@
 // you can write js here
 
-       // var kelvins = 294;
+       // let kelvins = 294;
         const prompt=require("prompt-sync")({sigint:true});
-        var kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
-        console.log(kelvins);
+        let kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+        //console.log(kelvins);
 
-        var celsius = kelvins - 273; // Je convertis la température kelvins en celsius et crée un variable celsius pour stocker le résultat. La température en Celsius est inférieure de 273 degrés à celle en Kelvins.
-        console.log(celsius);
+        let celsius = kelvins - 273; // Je convertis la température kelvins en celsius et crée une variable celsius pour stocker le résultat. La température en Celsius est inférieure de 273 degrés à celle en Kelvins.
+        //console.log(celsius);
 
-        var fahrenheits = celsius * (9/5) + 32; // Je calcule la température en Fahrenheits et crée un variable fahrenheits pour stocker le résultat de calcule.
-        console.log(fahrenheits);
+        /* ce code peut être remplacé par la ligne 19)
+        let fahrenheits = celsius * (9/5) + 32; // Je calcule la température en Fahrenheits et crée une variable fahrenheits pour stocker le résultat de calcule.
+        //console.log(fahrenheits);
 
         Math.floor(fahrenheits); // J'utilise la methode math.floor() pour arrondir le résultat obtenu.
-        console.log(Math.floor(fahrenheits));
+        //console.log(Math.floor(fahrenheits));
+        */
+
+        let fahrenheits = Math.floor(celsius * (9/5) + 32);
