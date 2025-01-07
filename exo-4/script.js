6b2559e4c89ca54266358322a09718c50a1af7d8
Xiaoyu-Julia <xiaoyu.ma@le-campus-numerique.fr>
2025-01-07 15:08:02 +0100

    exo-4: terminé

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,11 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+secretMessage.pop();
+secretMessage.push('to', 'program');
+secretMessage.splice(6, 5, 'right');
+secretMessage.shift();
+secretMessage.unshift('Programming');
+secretMessage.splice(5, 5, 'know');
+secretMessage.join(console.log('test'));
