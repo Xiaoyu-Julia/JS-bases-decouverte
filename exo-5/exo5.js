947a88acfa1344d0506d1cd35530b304504fbaf5
Xiaoyu-Julia <xiaoyu.ma@le-campus-numerique.fr>
2025-01-08 12:02:16 +0100

    exo-5, terminé

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -2,19 +2,19 @@
 
 console.log('exo-5');
 let input = 'Je suis heureuse.';
-//let upperInput = input.toUpperCase();
-//console.log(input.length);
+let upperInput = input.toUpperCase();
 //console.log(upperInput);
-const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+const vowels = ['A', 'E', 'I', 'O', 'U', 'Y'];
 let resultArray = [];
 
-for(let i = 0; i < input.length; i++){
+for(let i = 0; i < upperInput.length; i++){
         
-        console.log(input[i]);
-            let currentLetter = input[i];
+        console.log(upperInput[i]);
+            let currentLetter = upperInput[i];
             if(vowels.indexOf(currentLetter)>= 0){
                     resultArray.push(currentLetter);
                     console.log(resultArray);
+                    console.log(resultArray.join(''));
 
             }
             
