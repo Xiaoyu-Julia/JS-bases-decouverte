4ef747aa629d47265df4a413f5faed4af05eaf76
Xiaoyu-Julia <xiaoyu.ma@le-campus-numerique.fr>
2025-01-08 11:54:13 +0100

    exo-5

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,22 @@
 // you can write js here
 
 console.log('exo-5');
+let input = 'Je suis heureuse.';
+//let upperInput = input.toUpperCase();
+//console.log(input.length);
+//console.log(upperInput);
+const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+let resultArray = [];
+
+for(let i = 0; i < input.length; i++){
+        
+        console.log(input[i]);
+            let currentLetter = input[i];
+            if(vowels.indexOf(currentLetter)>= 0){
+                    resultArray.push(currentLetter);
+                    console.log(resultArray);
+
+            }
+            
+    }
+    
